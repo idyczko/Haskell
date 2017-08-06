329aa6426b0d001064363a43c4a0f91eabe2768f
Igor Dyczko <igor.dyczko@capgemini.com>
2017-08-06 20:02:38 +0200

    Finished chapter 1

--- chapter_1/functions.hs
+++ chapter_1/functions.hs
@@ -44,8 +44,41 @@ a_5 = null t -- checks if list is empty
 t_r = reverse t -- reverses table
 t_d = drop 3 t -- drops 3 first elements of a table (can be more than size - length (take 100 [1,2,3]) will return 3)
 minmax = minimum t == maximum t -- functions for finding minimal and maximal elements
--- sum, product functions
+-- sum, product - also list functions
+
 -- Ranges
 r_1 = ['A'..'z']
 r_2 = [1..10] -- Ranges are basically just a list (instead [1,2,3,4,5,6,7,8,9,10])
 r_3 = [1,3..10] -- Range with defined step
+r_4 = [0,0.1..5] -- As always - some errors caused by representation of floating point numbers - use with precaution!
+
+-- Infinite list
+rpt = repeat 4 -- Infinite list of fours [4,4,4,4,4...]
+rpt_2 = take 10 rpt -- List of ten fours
+rpct = replicate 3 10 -- List of three tens [10,10,10]
+
+-- List comprehension
+someComplicatedFunction tab threshold= [(2*x)^2|x<-tab, x>threshold] -- list comprehensions are cool!
+removeOdds tab = [x|x<-tab, not (odd x)] -- there is also "even"
+removeCapitals str = [x|x<-str, not (x `elem` ['A'..'Z']), x /= ' '] -- you can define multiple predicates! And yeah - did you remember string is just list of characters?
+length' tab = sum [1|_<-tab] -- _ means we don't care about specific elements - only think of table as a whole!
+removeEvens tab = [[x|x<-y, not (even x)]|y<-tab]
+
+-- Tuples - tuples are data structures that allow storing values of different types - they are not homogenous and they are of fixed length! tuple and triple are two different things
+tup = (12, "asdasd")
+first = fst tup -- first will be equal to 12
+second = snd tup -- yeah, you've guessed it - "asdasd"
+-- as tuple and triple are values of different types Haskell will not allow a list like this one: [(1,2), (1,2,3), (1,2)]
+equalTuples = (1,2,3) == (1,2,3) -- will be evaluated to true
+
+-- Creating pairs from lists:
+zipped = zip [1..] "Some bullshit" -- It just cuts the longer list off
+
+-- Extracting data from tuple:
+third (_,_,c,_) = c
+
+-- A cool list comprehension+triples assignment: which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24?
+triples = [(a,b,c)|a<-[1..10], b<-[1..10], c<-[1..10], c>b, b>a, a+b+c==24, a^2+b^2==c^2]
+
+-- Way smarter!
+triples' = [(a,b,c)|c<-[1..10], b<-[1..c], a<-[1..b], a+b+c==24, a^2+b^2==c^2]
