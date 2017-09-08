dbae8294cbeab01588fa6955ce7d0291aa1dee05
idyczko <igor.dyczko@gmail.com>
2017-09-08 22:57:42 +0200

    Primality check and any' function

--- chapter_4/functions.hs
+++ chapter_4/functions.hs
@@ -1,7 +1,7 @@
 -- Cheesy quicksort :) it is beautiful though :)
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
-quicksort (x:xs) = let smaller = [a | a <- xs, a <= x]; bigger = [a | a <- xs, a > x] in (quicksort smaller) ++ [x] ++ (quicksort bigger)
+quicksort (x:xs) = let smaller = [a | a <- xs, a <= x]; bigger = [a | a <- xs, a > x] in quicksort smaller ++ [x] ++ quicksort bigger
 
 -- And "elem'":
 elem' :: (Eq a) => a -> [a] -> Bool
@@ -9,3 +9,39 @@ a `elem'` [] = False -- Yep, we can also define function as infix.
 a `elem'` (x:xs)
    | a == x = True
    | otherwise = a `elem'` xs
+   
+-- Maximum of elements
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "The given list is empty"
+maximum' [x] = x
+maximum' (x:xs) = max' x (maximum' xs)
+       where max' a b
+                | a < b = b
+                | otherwise = a
+
+-- Replication of an element
+replicate' :: Int -> a -> [a]
+replicate' n x
+           | n <= 0 = []
+           | otherwise = x : replicate' (n-1) x
+
+-- Repeat
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+-- Take
+take' :: Int -> [a] -> [a]
+take' _ [] = []
+take' n (x:xs)
+        | n <= 0 = []
+        | otherwise = x : (take' (n-1) xs)
+
+
+-- Any 
+any' :: (a -> Bool) -> [a] -> Bool
+any' f [] = False
+any' f (x:xs) = f x || any f xs
+
+-- Primality test with function composition (.) and late function application ($)
+prime' :: Integer -> Bool
+prime' x =  not $ any' ((==0) . (x `mod`)) [pos | pos<-[2.. floor $ sqrt $ fromIntegral x]]
