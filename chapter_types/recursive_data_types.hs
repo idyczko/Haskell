5c04106a2a0d925e5e68b6f74d807cba6ef59331
idyczko <igor.dyczko@gmail.com>
2018-01-11 22:31:20 +0100

    Some additional binary tree operations implementend.

--- chapter_types/recursive_data_types.hs
+++ chapter_types/recursive_data_types.hs
@@ -14,7 +14,7 @@ y = 10 ^ 2 -^ 1 -- y = 10
 
 -- Let us implement something cool - a binary search tree!
 
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Read, Eq)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 -- Notice how it is actually a type constructor and not value constructor, as
 -- it contains type parameter! Let's create some cool tree:
 tree = Node 10 (Node 8 EmptyTree EmptyTree) (Node 20 EmptyTree EmptyTree)
@@ -35,12 +35,21 @@ findInTree x EmptyTree = Nothing
 findInTree x (Node n left right) = if x == n then Just n else
                                         if x < n then findInTree x left else findInTree x right
 -}
+
+
+getValue :: Tree a -> Maybe a
+getValue EmptyTree = Nothing
+getValue (Node x _ _) = Just x
+
+findNodeInTree :: (Ord a, Eq a) => a -> Tree a -> Tree a
+findNodeInTree x EmptyTree = EmptyTree
+findNodeInTree x tree@(Node y left right)
+              | x == y = tree
+              | x < y = findNodeInTree x left
+              | x > y = findNodeInTree x right
+
 findInTree :: (Ord a, Eq a) => a -> Tree a -> Maybe a
-findInTree x EmptyTree = Nothing
-findInTree x (Node y left right)
-              | x == y = Just y
-              | x < y = findInTree x left
-              | x > y = findInTree x right
+findInTree x tree = getValue $ findNodeInTree x tree
 
 numbers = [3,231,5,2,12,3,13,1,2123,1,33,12,23,123,6,3451,3,51,67,6,27]
 newTree = foldr insertValue EmptyTree numbers
@@ -49,8 +58,30 @@ inOrderTraversal :: (Show a, Eq a) => Tree a -> [Maybe a]
 inOrderTraversal EmptyTree = [Nothing]
 inOrderTraversal (Node x left right) = filter (/=Nothing) ((inOrderTraversal left) ++ [Just x] ++ (inOrderTraversal right))
 
--- Typeclasses 102
 
+inOrderTraversal' :: (Show a, Eq a) => Tree a -> [Maybe a]
+inOrderTraversal' EmptyTree = [Nothing]
+inOrderTraversal' (Node x left right) = filter (/=Nothing) ([Just x] ++ (inOrderTraversal left) ++ (inOrderTraversal right))
+
+reversedOrderTraversal :: (Show a, Eq a) => Tree a -> [Maybe a]
+reversedOrderTraversal = reverse . inOrderTraversal
+
+inverseBinaryTree :: Tree a -> Tree a
+inverseBinaryTree EmptyTree = EmptyTree
+inverseBinaryTree (Node x l r) = Node x (inverseBinaryTree r) (inverseBinaryTree l)
+
+getChildren :: Tree a -> [Maybe a]
+getChildren EmptyTree = [Nothing]
+getChildren (Node x left right) = [getValue left, getValue right]
+
+getChildren' :: [Tree a] -> [Maybe a]
+getChildren' (x:xs) = (getChildren x) ++ (getChildren' xs)
+
+--breadthFirstSearch :: Tree a ->
+
+-- Typeclasses 102
+{-
 instance (Show a) => Show (Tree a) where
   show EmptyTree = ""
   show (Node x left right) = show x
+-}
